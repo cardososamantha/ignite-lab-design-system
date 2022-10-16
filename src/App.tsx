31804d2a5cd495662ecb7e837280bdd1abe24461
Samantha Cardoso <samanthacardoso@Samantha-PC.localdomain>
2022-10-16 17:20:54 -0300

    initial-commit-and-configs

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css';
+
+export function App() {
+	return (
+		<>
+			<h1 className='font-bold text-2xl text-violet-500'>
+				Ignite Lab Design System
+			</h1>
+		</>
+	);
+}
